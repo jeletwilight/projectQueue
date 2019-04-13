d6667d9fc43f215ce004941c7b0a7232fde007a6
Kittitorn Rakpanyakaeo <koonjj@gmail.com>
2019-04-13 20:15:50 +0700

    update Edit Profile

--- app/src/main/java/com/example/jelelight/servicequeuing/EditProfileActivity.java
+++ app/src/main/java/com/example/jelelight/servicequeuing/EditProfileActivity.java
@@ -1,16 +1,24 @@
 package com.example.jelelight.servicequeuing;
 
+import android.app.DatePickerDialog;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +32,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class EditProfileActivity extends AppCompatActivity {
@@ -33,13 +42,17 @@ public class EditProfileActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
 
+    private Button calenView;
     private EditText nameEdit,heightEdit,weightEdit,birthEdit,cautionEdit,phoneEdit;
-    private RadioButton gendM,gendF,bloodA,bloodB,bloodO,bloodAB;
-
+    private RadioButton bGen,bBlood;
+    private RadioButton genM,genF,bloodA,bloodB,bloodAB,bloodO;
     private Button submitBtn,backBtn;
+    private RadioGroup rGend,rBlood;
+
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     private String userID;
-    private boolean hasKey;
+    //private boolean hasKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +61,13 @@ public class EditProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit_profile);
         bind();
         pageManage();
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                birthEdit.setText(dayOfMonth+"/"+month+"/"+year);
+            }
+        };
 
     }
 
@@ -73,18 +93,24 @@ public class EditProfileActivity extends AppCompatActivity {
         weightEdit = findViewById(R.id.weight_edit);
         cautionEdit = findViewById(R.id.caution_edit);
         phoneEdit = findViewById(R.id.phone_edit);
-        gendM = findViewById(R.id.gender_m);
-        gendF = findViewById(R.id.gender_f);
+        calenView = findViewById(R.id.select_date_btn);
+
+        genM = findViewById(R.id.gender_m);
+        genF = findViewById(R.id.gender_f);
         bloodA = findViewById(R.id.blood_a);
         bloodB = findViewById(R.id.blood_b);
-        bloodO = findViewById(R.id.blood_o);
         bloodAB = findViewById(R.id.blood_ab);
+        bloodO = findViewById(R.id.blood_o);
 
+        rGend = findViewById(R.id.radio_gend);
+        rBlood = findViewById(R.id.radio_blood);
         submitBtn = findViewById(R.id.editSubmit_Btn);
         backBtn = findViewById(R.id.backEdit_Btn);
 
         submitBtn.setOnClickListener(onClickListener);
         backBtn.setOnClickListener(onClickListener);
+
+        //birthEdit.setEnabled(false);
     }
 
     private void pageManage(){
@@ -107,13 +133,65 @@ public class EditProfileActivity extends AppCompatActivity {
                     startActivity(new Intent(EditProfileActivity.this,ProfileActivity.class));
                     finish();
                     break;
+                case R.id.birth_edit:
+                    onClickCalendar();
+                    break;
+                case R.id.select_date_btn:
+                    onClickCalendar();
+                    break;
                 case R.id.backEdit_Btn:
                     startActivity(new Intent(EditProfileActivity.this,ProfileActivity.class));
                     finish();
                     break;
             }
+
         }
     };
+
+    private void onClickCalendar(){
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dialog = new DatePickerDialog(
+                EditProfileActivity.this,
+                android.R.style.Theme_Holo_Dialog_MinWidth,
+                mDateSetListener,year,month,day
+        );
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+    }
+
+    private void genderCheck(){
+        int radioId = rGend.getCheckedRadioButtonId();
+        bGen = findViewById(radioId);
+        //Toast.makeText(this,bGen.getText(),Toast.LENGTH_SHORT).show();
+        if(bGen.getText().equals("Male")){
+            mReferenceUser.child(mUser.getUid()).child("gender").setValue("Male");
+        }else if(bGen.getText().equals("Female")){
+            mReferenceUser.child(mUser.getUid()).child("gender").setValue("Female");
+        }else{
+            mReferenceUser.child(mUser.getUid()).child("gender").setValue("NULL");
+        }
+    }
+
+    private void bloodCheck(){
+        int radioId = rBlood.getCheckedRadioButtonId();
+        bBlood = findViewById(radioId);
+        //Toast.makeText(this,bBlood.getText(),Toast.LENGTH_SHORT).show();
+        if(bBlood.getText().equals("A")){
+            mReferenceUser.child(mUser.getUid()).child("blood").setValue("A");
+        }else if(bBlood.getText().equals("B")){
+            mReferenceUser.child(mUser.getUid()).child("blood").setValue("B");
+        }else if(bBlood.getText().equals("AB")){
+            mReferenceUser.child(mUser.getUid()).child("blood").setValue("AB");
+        }else if(bBlood.getText().equals("O")){
+            mReferenceUser.child(mUser.getUid()).child("blood").setValue("O");
+        }else{
+            mReferenceUser.child(mUser.getUid()).child("blood").setValue("NULL");
+        }
+    }
 
     private void readProfile(){
         mReferenceUser.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -128,6 +206,26 @@ public class EditProfileActivity extends AppCompatActivity {
                         checkProfileValue(keyNode,"height",heightEdit);
                         checkProfileValue(keyNode,"weight",weightEdit);
                         checkProfileValue(keyNode,"caution",cautionEdit);
+                        if(keyNode.child("blood").exists()){
+                            String b = keyNode.child("blood").getValue().toString();
+                            if(b == "A"){
+                                bloodA.setChecked(true);
+                            }else if(b == "B"){
+                                bloodB.setChecked(true);
+                            }else if(b == "AB"){
+                                bloodAB.setChecked(true);
+                            }else if(b == "O"){
+                                bloodO.setChecked(true);
+                            }
+                        }
+                        if(keyNode.child("gender").exists()){
+                            String g = keyNode.child("gender").getValue().toString();
+                            if(g == "Male"){
+                                genM.setChecked(true);
+                            }else if(g == "Female"){
+                                genF.setChecked(true);
+                            }
+                        }
                     }
                 }
             }
@@ -140,9 +238,16 @@ public class EditProfileActivity extends AppCompatActivity {
     }
 
     private void saveProfile(){
-        mReferenceCurrentUser.child("name").setValue(String.valueOf(nameEdit.getText()));
-        UserProfileChangeRequest req = new UserProfileChangeRequest.Builder().setDisplayName(String.valueOf(nameEdit.getText())).build();
-        mUser.updateProfile(req);
+
+        genderCheck();
+        bloodCheck();
+
+        if(!nameEdit.getText().equals("") || !nameEdit.getText().equals(null)){
+            mReferenceCurrentUser.child("name").setValue(String.valueOf(nameEdit.getText()));
+            UserProfileChangeRequest req = new UserProfileChangeRequest.Builder().setDisplayName(String.valueOf(nameEdit.getText())).build();
+            mUser.updateProfile(req);
+        }
+
     }
 
     private void checkProfileValue(DataSnapshot node, String child, EditText tv){
